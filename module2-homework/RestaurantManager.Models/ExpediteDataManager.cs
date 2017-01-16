72a2cb1bf4a4aa7cfff661adc7fd0961a0ed048f
baziili <vbaziili@yahoo.com>
2017-01-16 14:23:50 +0200

    module 2 - homework added
    was added homework folder for module 2

---
+++ module2-homework/RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            //OnPropertyChanged("OrderItems");
+            this.OrderItems = base.Repository.Orders;
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+            set { this.OnPropertyChanged();}
+        }
+    }
+}
---
+++ module2-homework/RestaurantManager.Models/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManager.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
