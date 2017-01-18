e732fe9d0c8ca8327021a3cc16f1382b44ccfa61
baziili <vbaziili@yahoo.com>
2017-01-18 10:53:42 +0200

    homework - module 3
    was added homework for module 3 - dev206.2x

---
+++ module3-homework/RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior:DependencyObject,IBehavior
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+            }
+        }
+
+        public void Detach()
+        {
+            if (this.AssociatedObject != null && this.AssociatedObject is Page)
+            {
+                this.AssociatedObject = null;
+                (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+            }
+        }
+
+        private async void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message,this.Title).ShowAsync();
+        }
+
+        public DependencyObject AssociatedObject { get; private set; }
+    }
+}
